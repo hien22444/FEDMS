eccb4112df0ea01d48655b500b4c9b6e0808b001
thanhtungf123 <thanhtungphankt@gmail.com>
2026-02-03 15:15:01 +0700

    update admin UI

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,25 +2,19 @@ export const ROUTES = {
   LANDING: '/',
   SIGN_IN: '/signin',
   SIGN_UP: '/signup',
-  GOOGLE_CALLBACK: '/auth/google/callback',
-
-  // Student routes
-  STUDENT_DASHBOARD: '/student/dashboard',
-  STUDENT_NEWS: '/student/news',
-  STUDENT_SCHEDULE: '/student/schedule',
-  STUDENT_BOOKING: '/student/booking',
-  STUDENT_UTILITIES: '/student/utilities',
-  STUDENT_PAYMENT: '/student/payment',
-  STUDENT_REQUESTS: '/student/requests',
-  STUDENT_MAINTENANCE: '/student/maintenance',
-  STUDENT_PROFILE: '/student/profile',
-
-  // Security routes
-  SECURITY_DASHBOARD: '/security',
+  // Security
+  DASHBOARD: '/security',
   CAMERA_CHECKIN: '/security/camera-checkin',
   CHECKOUT_REQUESTS: '/security/checkout-requests',
   VISITORS: '/security/visitors',
-
+  PROJECTS: '/projects',
+  RENDER: '/render',
+  // Admin routes
+  ADMIN: '/admin',
+  ADMIN_FACILITIES: '/admin/facilities',
+  ADMIN_USERS: '/admin/users',
+  ADMIN_REPORTS: '/admin/reports',
+  ADMIN_DATA: '/admin/data',
   // Manager routes
   MANAGER: '/manager',
   MANAGER_DORMS: '/manager/dorms',
